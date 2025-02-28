3b8cf9d20bb15b42ac353ed68e9caa65a91f77b8
Damith Bandara <54299503+damith-bandara@users.noreply.github.com>
2025-02-28 19:03:19 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,8 +1,5 @@
-
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-    }
-  }
+provider "aws" {
+  region  = "ap-southeast-1"  # Specify your AWS region here, e.g., us-east-1
+  access_key = var.AWS_ACCESS_ID    # Referencing environment variables or Terraform variable
+  secret_key = var.AWS_SECRET_KEY
 }
