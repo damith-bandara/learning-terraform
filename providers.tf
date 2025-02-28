be9e9b117d8b1ed367a9b93509d9d9381e779215
Damith Bandara <54299503+damith-bandara@users.noreply.github.com>
2025-02-28 19:18:11 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region  = "ap-southeast-1"  # Specify your AWS region here, e.g., us-east-1
-  access_key = var.AWS_ACCESS_ID    # Referencing environment variables or Terraform variable
-  secret_key = var.AWS_SECRET_KEY
+  access_key = "AKIA3RYC5S3N3D3GETGH"    # Referencing environment variables or Terraform variable
+  secret_key = "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"
 }
